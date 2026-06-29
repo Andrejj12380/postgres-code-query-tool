d2936facb15f876508a29de1afaebaf833153762
Андрей Худояров <andrei.hudojaroff@yandex.ru>
2026-06-29 09:08:49 +0400

    build: add server bundle and Windows installer executable

--- build/server.cjs
+++ build/server.cjs
@@ -30105,8 +30105,8 @@ app.post("/api/settings", async (req, res) => {
 });
 var CURRENT_VERSION = "2.1.1";
 try {
-  if ("2.1.15") {
-    CURRENT_VERSION = "2.1.15";
+  if ("2.1.16") {
+    CURRENT_VERSION = "2.1.16";
   } else {
     const pkgPath = import_path.default.join(process.cwd(), "package.json");
     if (import_fs.default.existsSync(pkgPath)) {
